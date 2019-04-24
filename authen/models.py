55b1c387d57f58af6b84ca455ddcc1cec1c5ce29
thecook <bbster12@gmail.com>
2019-04-25 00:38:50 +0900

    사용자정의 User 모델 만듬

--- authen/models.py
+++ authen/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Member(models.Model):
+class Member(AbstractUser):
     user_id = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
--- cook_intra/settings_base.py
+++ cook_intra/settings_base.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'feeds',
 ]
 
+AUTH_USER_MODEL = 'authen.Member'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
