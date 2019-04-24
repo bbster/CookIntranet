2b45c3fc23c57c3371a294668e0290c633675ff9
thecook <bbster12@gmail.com>
2019-04-25 00:41:40 +0900

    blank=True, null=True 옵션 추가함.
    DB 마이그레이션 할 때 예상치못한 빈값, null 값들 때문에 잘 안되는 문제를 미연에 방지하기 위해서

--- authen/models.py
+++ authen/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Member(AbstractUser):
-    user_id = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=30)
+    user_id = models.CharField(max_length=30, blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    phone_number = models.CharField(max_length=30, blank=True, null=True)
