922631d4818b2bd7f61d1e9940084ab8c94d269c
thecook <bbster12@gmail.com>
2019-04-25 00:54:43 +0900

    기존 유저모델을 상속하면서 중복된 필드 제거

--- authen/models.py
+++ authen/models.py
@@ -3,7 +3,4 @@ from django.db import models
 
 
 class Member(AbstractUser):
-    user_id = models.CharField(max_length=30, blank=True, null=True)
-    first_name = models.CharField(max_length=30, blank=True, null=True)
-    last_name = models.CharField(max_length=30, blank=True, null=True)
     phone_number = models.CharField(max_length=30, blank=True, null=True)
