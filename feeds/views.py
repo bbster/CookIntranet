ea8c447af622b53a89860e21fe871343a5988e81
thecook <bbster12@gmail.com>
2019-05-07 10:17:49 +0900

    FEED CRUD

--- feeds/views.py
+++ feeds/views.py
@@ -7,11 +7,11 @@ from .models import Feed
 
 
 class FeedViewSet(ModelViewSet):
-    serializer_class = FeedSerializer
     queryset = Feed.objects.all()
+    serializer_class = FeedSerializer
     permission_classes = (feedpermissions.BasePermission,)
 
-    @action(detail=False, methods=['POST'])
+    @action(detail=False, methods=['GET'])
     def feedlist(self, request, *args, **kwargs):
         param_created_at = request.query_params.get('created_at', None)  # request.query_params -> request.GET
         if param_created_at:
@@ -24,11 +24,14 @@ class FeedViewSet(ModelViewSet):
                 self.queryset = self.queryset.filter(created_date__date=splited[0])
         return super().list(request, *args, **kwargs)
 
+    @action(detail=False, methods=['POST'])
     def createfeed(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
+    @action(detail=False, methods=['POST'])
     def updatefeed(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
+    @action(detail=False, methods=['POST'])
     def deletefeed(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
