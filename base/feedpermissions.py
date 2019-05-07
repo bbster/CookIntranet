3d6339ee68bb4ffbbb9094aa82c6f077fc77dc8a
thecook <bbster12@gmail.com>
2019-05-07 10:47:01 +0900

    테스트용 퍼미션 주석처리

--- base/feedpermissions.py
+++ base/feedpermissions.py
@@ -9,16 +9,16 @@ class BasePermission(DRFBasePermission):
         if view.action == "feedlist":             # view.action  feed list
             return True  # permission True
         elif view.action == "createfeed":  # view.decode_jwt_token 조인
-            token = request.data.get("token", None)
-            if not token:
-                return False
-            try:
-                user_info = jwt.decode(token, settings_base.SECRET_KEY, algorithms=['HS256'])  # jwt token decode
-                user = Member.objects.get(user_info["username"])
-                request.user = user
-                return True
-            except Exception as e:
-                return False
+            # token = request.data.get("token", None)
+            # if not token:
+            #     return False
+            # try:
+            #     user_info = jwt.decode(token, settings_base.SECRET_KEY, algorithms=['HS256'])  # jwt token decode
+            #     user = Member.objects.get(user_info["username"])
+            #     request.user = user
+            #     return True
+            # except Exception as e:
+            #     return False
             return true
 
         elif view.action == "updatefeed":  # view.decode_jwt_token 조인
--- schedular/views.py
+++ schedular/views.py
@@ -26,15 +26,17 @@ class ScheduleViewSet(ModelViewSet):
                 self.queryset = self.queryset.filter(created_date__date=splited[0])
         return super().list(request, *args, **kwargs)
 
+    @action(detail=False, methods=['POST'])
     def createschedule(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return super().create(request, *args, **kwargs)
 
+    @action(detail=False, methods=['POST'])
     def updateschedule(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
+        serializer = self.serializer_class(data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @action(detail=False, methods=['POST'])
     def deleteschedule(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
